6738b6182fac345f85037d170c1d0e89e65fe11c
Elias Eberhardss <elias.eberhardsson@gmail.com>
2017-12-23 21:28:06 +0100

    first commit

---
+++ init.el
@@ -0,0 +1,14 @@
+(require 'package) ;; You might already have this line
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
+  (add-to-list 'package-archives (cons "melpa" url) t))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize) ;; You might already have this line
+
+(tool-bar-mode -1)
+
+(when (fboundp 'windmove-default-keybindings)
+  (windmove-default-keybindings))
